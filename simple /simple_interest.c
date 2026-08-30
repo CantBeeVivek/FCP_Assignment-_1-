040ae9c0131a8e99a5aaf002750178f6d1735ed7
CantBeeVivek <contact.vivekpatel13@gmail.com>
2026-08-31 00:03:19 +0530

    Update simple_interest.c

--- simple /simple_interest.c
+++ simple /simple_interest.c
@@ -1,18 +1,1 @@
-#include <stdio.h>
 
-int main() {
-    float p,r,t,si;
-    printf("Enter the principal amount: ");
-    scanf("%f", &p);
-    printf("Enter the annual rate of interest : ");
-    scanf("%f", &r);
-    printf("Enter the time period: ");
-    scanf("%f", &t);
-    si = (p*r* t)/100.0;
-    printf("\n--- Summary ---\n");
-    printf("Principal Amount : %.2f\n", p);
-    printf("Interest Rate    : %.2f%%\n", r);
-    printf("Time Period      : %.2f years\n", t);
-    printf("Simple Interest  : %.2f\n", si);
-    return 0;
-}
