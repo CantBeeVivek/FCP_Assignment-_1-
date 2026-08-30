9f81399ac9fe9529b70b24f2510b3bbf4211fb9b
CantBeeVivek <contact.vivekpatel13@gmail.com>
2026-08-30 23:42:07 +0530

    Create ASCII_Code.c

---
+++ ASCII_Code.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int main()
+{
+    printf("PRINT ASCII CODE OF ANY CHARACTER\n");
+    char c;
+    printf("Enter your Character : ");
+    scanf("%c", &c);
+    printf("The Character's ASCII Code is : %d",c);
+}
