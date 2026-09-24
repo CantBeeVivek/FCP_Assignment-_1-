b220f45e43b4b9e5324ed475b4b3ec9bce5eb5b1
CantBeeVivek <contact.vivekpatel13@gmail.com>
2026-09-25 02:04:49 +0530

    Update Q_40.c

--- Assignment_1/Q_40.c
+++ Assignment_1/Q_40.c
@@ -1,7 +1,8 @@
 #include <stdio.h>
 int main()
 {
-    int ls[14],sum=0,i;
+    /*
+        int ls[14],sum=0,i;
     printf("Enter the numbers in the list : \n");
     for(i=0; i<=13; i++){
         scanf("%d",&ls[i]);
@@ -14,4 +15,19 @@ int main()
         }
     }
     printf("SUM = %d", sum);
+    */
+    
+    
+    int a,b, sum=0,i,n;
+    printf("Enter the number of numbers in the list : ");
+    scanf("%d", &n);
+    printf("Enter the numbers in the list : \n");
+    for(i=1; i<=n; i++){
+        scanf("%d", &a);
+        sum = sum + a;
+        if(a<0){
+            break;
+        }
+    }
+    printf("The sum of the terms in the list is %d", sum);
 }
